f3877170fe1cc0d2c829afa84ad2fa580e9567e7
Niraj Kumar Jha <niraj.jha@gojek.com>
2022-09-12 21:43:24 +0530

    Initial Commit

---
+++ NaviGit_Niraj/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NaviGit_Niraj
+//
+//  Created by Niraj Kumar Jha on 12/09/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ NaviGit_NirajUITests/NaviGit_NirajUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  NaviGit_NirajUITestsLaunchTests.swift
+//  NaviGit_NirajUITests
+//
+//  Created by Niraj Kumar Jha on 12/09/22.
+//
+
+import XCTest
+
+class NaviGit_NirajUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
